0ce7a1a87c737f087b0bb45cf5fecf202e67ad89
Tina Johnson <tinajohnson.1234@gmail.com>
2016-04-12 18:04:52 +0530

    Add sorting by price feature

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -3,30 +3,34 @@
  */
 var app = angular.module( 'shopApp', [] );
 app.controller( 'mainController', function( $scope,$http ) {
+    var count = 0;
     $scope.contents =  null;
     $scope.added = [];
     var Totprice = 0;
+    $scope.predicate = 'price';
 
     $http.get( 'data/category.json' )
         .success( function( data ) {
             $scope.contents = data;
         })
+
+    $scope.order = function(predicate) {
+        $scope.reverse = ($scope.predicate === predicate) ? false : true;
+
+    };
+
     $scope.addToCart = function( name, price ) {
         var item= {
             name: name,
             price: price
         };
         Totprice = parseInt(Totprice) + parseInt(item.price);
-        $scope.added.push( item );
-        if($scope.added == null) {
-            $('#checkoutModal').modal('hide');
-        }
 
         $scope.tax = 0.10 * Totprice;
         $scope.total = $scope.tax + 300 + Totprice;
-        window.onload = function() {
-            localStorage.setItem("data", $scope.added);
-        }
+        //window.onload = function() {
+        //    localStorage.setItem("data", $scope.added);
+        //}
     }
 
 
